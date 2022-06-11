2c8b5220fb854eb8355bad22274b0917492cb92c
Ricardo Nisiide <rnisiide@gmail.com>
2022-06-11 17:22:46 -0300

    finished the etch-a-skectch project after many changes and learning many new things! the process is in the readme file.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # etch-a-sketch
 The Odin Project: PROJECT: ETCH-A-SKETCH
+
+This project took me a bit longer than expected... Mainly because I learned so much while developing it!
+
+At first i was struggling with the grid itself. With the help of Coding with Rob (https://www.youtube.com/watch?v=dyhuaXeuyGo&t=755s&ab_channel=CodingWithRob) I was able to understand it.
+
+Then, i started choosing which colors to add buttons...Black, white, green, yellow and so on.
+I also added size of canvas for Small, Medium and Large, and a text input field to change the density of the canvas (rows x columns)
+Each color and size had a button, which was making the code and the design bloated. 
+
+So I looked at some options to add a color picker, instead of a button for each color. At first i added an RGB slider! I hadn't used it before, so it was good to learn how to use range input in HTML and how to manipulate it with JavaScript. I also changed the canvas size and pixel density to a range slider, which makes it more user friendly.
+
+I was happy with the structure, when I learned about the 'color' input in HTML, which i also didn't use before! So i changed the RGB range slider to the Color input, which made the design cleaner and the java script code MUCH shorter, as i didn't need a variable and document selector for each input (R, G and B) anymore. 
+
+Lastly, i went to add the Rainbow button. The function I also got from the help of Coding with Rob, but I decided to add it as a On/Off button. So with that I also learned the 'checkbox' input!
+
+And finally, on to CSS! Which was another great challenge. How to change all the input buttons on CSS was something really new and confusing. But I think i started to get the hang of it.
+
+
+This was a really fun project to develop, as I learned a lot with all the changes I decided to make. There's still some things that could be improved and added, like a 'Clear' button, grid on and off toggle, eraser button. But I'll revisit it in a later time for these improvements.  
--- script.js
+++ script.js
@@ -1,111 +1,59 @@
-let color = 'black';
+let color = "black";
 let background = 'white';
-const board = document.querySelector('.board')
-populateBoard(16);
+const board = document.querySelector('.board');
+const rainbow = document.querySelector('#rainbow');
+const mouseColor = document.getElementById('mouseColor');
+const backColor = document.getElementById('backColor');
+const backgroundInput = document.querySelector('.backgroundInput');
 
-
-/*<button id="small">Small</button>
-                <button id="medium">Medium</button>
-                <button id="large">Large</button>
-*/
-
-const small = document.querySelector('#small');
-small.addEventListener('click', () => {
-  board.style.width = '300px';
-  board.style.height = '300px';
+backgroundInput.addEventListener('change', function() {
+  board.style.backgroundColor = `${backgroundInput.value}`;
+  backColor.style.backgroundColor = `${backgroundInput.value}`;
+  populateBoard(slide.value);
 })
 
-const medium = document.querySelector('#medium');
-medium.addEventListener('click', () => {
-  board.style.width = '500px';
-  board.style.height = '500px';
-})
-
-
-const large = document.querySelector('#large');
-large.addEventListener('click', () => {
-  board.style.width = '800px';
-  board.style.height = '800px';
-})
-
-
-const btnBlack = document.querySelector('#black');
-btnBlack.addEventListener('click', () => 
-  {color = 'black'})
-
-const backBlack = document.querySelector('#backblack');
-backBlack.addEventListener('click', () => 
-  {background = 'black'; returnValue()});
-
-const btnBlue = document.querySelector('#blue');
-btnBlue.addEventListener('click', () => 
-  {color = 'blue'})
-
-const backBlue = document.querySelector('#backblue');
-backBlue.addEventListener('click', () => 
-    {background = 'blue'; returnValue()});
-
-const btnRandom = document.querySelector('#random');
-btnRandom.addEventListener('click', () => {
-  let randomClick = Math.floor(Math.random()*16777215).toString(16);
-  color = `#${randomClick}`})
-
-const backRandom = document.querySelector('#backrandom');
-backRandom.addEventListener('click', () => {
-  let randomClick = Math.floor(Math.random()*16777215).toString(16);
-  background = `#${randomClick}`; returnValue()})
-
-const btnWhite = document.querySelector('#white');
-btnWhite.addEventListener('click', () => 
-  {color = 'white'})
-
-const backWhite = document.querySelector('#backwhite');
-backWhite.addEventListener('click', () => 
-  {background = 'white'; returnValue()});
-
-const btnOrange = document.querySelector('#orange');
-btnOrange.addEventListener('click', () => 
-    {color = 'orange'})
-
-const backOrange = document.querySelector('#backorange');
-backOrange.addEventListener('click', () => 
-  {background = 'orange'; returnValue()});
-
-const btnRed = document.querySelector('#red');
-btnRed.addEventListener('click', () => 
-  {color = 'red'})
-
-
-const backRed = document.querySelector('#backred');
-backRed.addEventListener('click', () => 
-  {background = 'red'; returnValue()});
-        
-const btnYellow = document.querySelector('#yellow');
-btnYellow.addEventListener('click', () => 
-    {color = 'yellow'})
-
-
-const backYellow = document.querySelector('#backyellow');
-backYellow.addEventListener('click', () => 
-  {background = 'yellow'; returnValue()});
-
-
-const btnGreen= document.querySelector('#green');
-btnGreen.addEventListener('click', () => 
-    {color = 'green'})
-
-
-const backGreen = document.querySelector('#backgreen');
-backGreen.addEventListener('click', () => 
-  {background = 'green'; returnValue()});   
-
-function returnValue() {
-  let input = document.getElementById("userInput").value;
-  if (input >= 2 && input <= 100) {
-    populateBoard(input)} else {alert(
-      'Please select a number from 2 to 100. Negative numbers not allowed')}
+function changeColor(e) {
+  if (rainbow.checked) {
+    e.target.style.background = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {e.target.style.background = `${mouseInput.value}`}
 };
 
+
+let mouseInput = document.querySelector('.mouseInput');
+
+rainbow.addEventListener('click', (e) => {
+  if (e.target.checked) {mouseColor.style.background = `linear-gradient(
+    45deg,
+    rgba(255, 0, 0, 1) 0%,
+    rgba(255, 154, 0, 1) 10%,
+    rgba(208, 222, 33, 1) 20%,
+    rgba(79, 220, 74, 1) 30%,
+    rgba(63, 218, 216, 1) 40%,
+    rgba(47, 201, 226, 1) 50%,
+    rgba(28, 127, 238, 1) 60%,
+    rgba(95, 21, 242, 1) 70%,
+    rgba(186, 12, 248, 1) 80%,
+    rgba(251, 7, 217, 1) 90%,
+    rgba(255, 0, 0, 1) 100%)`
+  } else {mouseColor.style.background = `${mouseInput.value}`}})
+
+mouseInput.addEventListener('change', function() {
+  if (rainbow.checked) {
+    color = `hsl(${Math.random() * 360}, 100%, 50%)`
+    } else {color = `${mouseInput.value}`; mouseColor.style.background = `${mouseInput.value}`;
+}})
+
+let slide = document.getElementById("myRange");
+slide.onchange = function () {
+  populateBoard(this.value);
+}
+
+
+let canvas = document.getElementById("myCanvas");
+canvas.onchange = function () {
+  board.style.width = `${this.value}px`;
+  board.style.height = `${this.value}px`;
+} 
 
 function populateBoard(size){
   let squares = board.querySelectorAll('div')
@@ -117,16 +65,12 @@ function populateBoard(size){
 
   for (let i = 0; i < totalSquares; i++) {
     let square = document.createElement('div')
-    square.addEventListener('mouseover', () => {
-      square.style.background = `${color}`;
-      
-    }
-  
-    ) 
-    square.style.background = `${background}`
+    square.addEventListener('mouseover', changeColor) 
     square.style.borderStyle = 'solid';
-    square.style.borderColor = '#9c9c9c';
+    square.style.borderColor = 'rgb(230, 230, 230)';
     square.style.borderWidth = '1px';
     board.insertAdjacentElement ('beforeend', square)
 }}
 
+
+populateBoard(16);
